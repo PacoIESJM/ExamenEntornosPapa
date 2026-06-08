b8ae92c13d0a9dbabfb64d4d668615ccced52d48
Paco <dummy@paco.com>
2026-06-08 12:48:36 +0200

    Coordinador sumado

---
+++ src/visitapapa/CoordinadorEvento.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package visitapapa;
+
+/**
+ *
+ * @author Pacoep
+ */
+import java.util.Scanner;
+
+public class CoordinadorEvento {
+
+    private String nombre;
+    private int id;
+    private Especialidad especialidad;
+
+    public CoordinadorEvento(String nombre, int id, Especialidad especialidad) {
+        this.nombre = nombre;
+        this.id = id;
+        this.especialidad = especialidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Especialidad getEspecialidad() {
+        return especialidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
+
